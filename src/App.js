3d1352f05889e1bffd5e7167b6a07d421c276871
Jorge Figueroa <jorgefigueroa@Jorges-MacBook-Pro.local>
2021-11-13 07:52:16 -0500

    added a typing effect & logic to when we render the Typewriter

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import About from './Components/About';
 import Contact from './Components/Contact';
 import Footer from './Components/Footer';
-import { Hero } from './Components/Hero';
+import Hero  from './Components/Hero';
 import { Navbar } from './Components/Navbar';
 import Projects from './Components/Projects';
 import Work from './Components/Work';
--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,24 +1,100 @@
-import React from "react";
+import React, { Component } from 'react'
 import styled from "styled-components";
 import Button from "./Button";
+import Typewriter from 'typewriter-effect';
+export default class Hero extends Component {
 
-export const Hero = () => {
-  return (
-    <HeroContainer>
-      <h1>My name is</h1>
-      <h2>Jorge Figueroa.</h2>
-      <h3>I build things for the web.</h3>
-      <p>
-        I'm a Software Engineering student from Rochester, NY. Studying to be a
-        Full-Stack Engineer.
-      </p>
-      <Button
-        bttnText="Get In Touch"
-        link="mailto:softwarebyjorge@gmail.com"
-      ></Button>
-    </HeroContainer>
-  );
-};
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: 0,
+      isOn: false,
+      start: 0
+    }
+    this.startTimer = this.startTimer.bind(this);
+    this.stopTimer = this.stopTimer.bind(this);
+    this.resetTimer = this.resetTimer.bind(this);
+  }
+
+  startTimer() {
+    this.setState({
+      isOn: true,
+      time: this.state.time,
+      start: Date.now() - this.state.time
+    })
+    this.timer = setInterval(() => this.setState({ 
+      time: Date.now() - this.state.start
+    }), 1)
+  }
+
+  stopTimer() {
+    this.setState({
+      isOn: false,
+    })
+    clearInterval(this.timer)
+  }
+
+  resetTimer() {
+    this.setState({
+      time: 0,
+      isOn: false
+    })
+  }
+
+  componentDidMount() {
+    this.startTimer();
+    // this makes a call once the component has been render but we give it a timeout to ensure the entire component has loaded.
+    setTimeout(() => {
+      this.stopTimer();
+    }, 10000)
+  }
+
+  componentWillUnmount() {
+    this.stopTimer();
+  }
+
+  render() {
+    return (
+      <HeroContainer>
+        <div className='greeting'> 
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter.typeString('Hi, my name is')
+                .pauseFor(2500)
+                .start();
+            }}
+          />
+        </div>
+        <div className='name'> 
+          { (this.state.time > 2500) ? <Typewriter
+            onInit={(typewriter) => {
+              typewriter.typeString('Jorge Figueroa.')
+                .pauseFor(3000)
+                .start();
+            }}
+          /> : null}
+        </div>
+        <div className='intro'> 
+          { (this.state.time > 4700) ? <Typewriter
+            onInit={(typewriter) => {
+              typewriter.typeString('I build things for the web.')
+                .pauseFor(3500)
+                .start();
+            }}
+          /> : null}
+        </div>
+        <p>
+          I'm a Software Engineering student from Rochester, NY. Studying to be a
+          Full-Stack Engineer.
+        </p>
+        <Button
+          bttnText="Get In Touch"
+          link="mailto:softwarebyjorge@gmail.com"
+        ></Button>
+      </HeroContainer>
+    );
+  }
+}
 
 const HeroContainer = styled.div`
   margin-top: 40%;
@@ -26,17 +102,17 @@ const HeroContainer = styled.div`
   align-items: flex-start;
   min-height: 50vh;
 
-  h1 {
+  .greeting {
     margin: 0 0 5px 4px;
     color: #ffc09f;
     font-size: 1.5rem;
     font-weight: 400;
   }
-  h2 {
+  .name {
     color: #0496ff;
     font-size: 4rem;
   }
-  h3 {
+  .intro {
     margin-top: 10px;
     color: #2af5ff;
     line-height: 0.9;
